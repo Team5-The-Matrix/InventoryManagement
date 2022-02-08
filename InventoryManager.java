f36e6cf87dd90e328f457de2a716b6e31993bdda
noahpk2 <noah@pearsonkramer.com>
2022-02-08 10:11:57 -0700

    Basic sketch of InventoryManager

--- InventoryManager.java
+++ InventoryManager.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Hashtable;
 
 public class InventoryManager{
@@ -11,30 +12,101 @@ public class InventoryManager{
     // userType: 0 - Admin, 1 - Employee, 2 - Customer
 
     public static void InventoryManager (String csvPath, boolean authenticated, int userType ) throws IOException,FileNotFoundException{
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         boolean run = true;
         while(run){
 
             //open initial csv
-        Hashtable<String,String[]> inventoryTable = uploadCsv(csvPath);
+            Hashtable<String,String[]> inventoryTable = uploadCsv(csvPath);
+
+            //which type of access?
+
+            if(userType == 0) //admin
+            {
+
+                System.out.println("Admin Access - Inventory System");
+                System.out.println("Enter a choice: ");
+                System.out.println("0 - Search an entry (read) ");
+                System.out.println("1 - Create an entry (new inventory item)");
+                System.out.println("2 - Update an entry (change inventory number, prices ) ");
+                System.out.println("3 - Delete an entry (delete an inventory item entirely) ");
+                System.out.println("4 - Exit system "); // back to main? or fully exit
 
 
-        // prompt user ? Or GUI?
+                switch(Integer.parseInt(br.readLine())){                
+                case 0:
+                    
+                case 1:
 
+                case 2:
 
+                case 3:
 
-
-
-
-
+                case 4:{
+                    run = false;
+                    break;
+                }    
+                default:  
+                }
+            }
+            else if (userType ==1) //Employee
+            {
+                System.out.println("Employee Access - Inventory System");
+                System.out.println("Enter a choice: ");
+                System.out.println("0 - Search an entry (read) ");
+                System.out.println("1 - Create an entry (new inventory item)");
+                System.out.println("2 - Update an entry (change inventory number, prices ) ");
+                System.out.println("3 - Delete an entry (delete an inventory item entirely) ");
+                System.out.println("4 - Exit system "); // back to main? or fully exit
+                switch(Integer.parseInt(br.readLine())){                
+                    case 0:
+                        
+                    case 1:
+    
+                    case 2:
+    
+                    case 3:
+    
+                    case 4:
+                    
+                    default:  
+                }
+            }
+            else if(userType ==2) //customer
+            {
+                System.out.println("Customer Access - Inventory System");
+                System.out.println("Enter a choice: ");
+                System.out.println("0 - Search an entry (read) ");
+                System.out.println("1 - Purchase an item ");
+                System.out.println("2 - Exit system "); // back to main? or fully exit
+                switch(Integer.parseInt(br.readLine())){                
+                    case 0:
+                        
+                    case 1:
+    
+                    case 2:
+                    
+                    default:  
+                }
+            }
         }
     }
+
+
+    public static void search(int userType,Hashtable<String,String[]> inventoryTable,String itemID)
+    {
+
+
+
+    }
+
+
 
     //uploadCsv : method to read initial csv file into a hashTable for fast search
     static Hashtable<String,String[]> uploadCsv(String csvPath) throws FileNotFoundException, IOException{
         
         BufferedReader br = new BufferedReader(new FileReader(csvPath));
-        Hashtable<String,String[]> inventoryTable = new Hashtable<String,String[]>();
+        Hashtable<String, String[]> inventoryTable = new Hashtable<String,String[]>();
         while (br.readLine()!= null)
         {
             String line;
@@ -52,9 +124,12 @@ public class InventoryManager{
         return inventoryTable;
     }
 
-    static void Create() {
+    static void Create(Hashtable<String,String[]> inventoryTable) {
 
         //team write this method
+
+        //hashtable: create a string for Item ID, and a string array for other info. use inventoryTable.put(String, String[])
+
 
     }
 
@@ -63,7 +138,7 @@ public class InventoryManager{
         //team write this method
 
     }
-    static void Read(){
+    static void Read(int userType,Hashtable<String,String[]> inventoryTable,String itemID){
 
         //team write this method
 
