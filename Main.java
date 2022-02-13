9eedb294cd54f31246411069ceebfccb765619fe
milagrosher11 <89418703+milagrosher11@users.noreply.github.com>
2022-02-13 15:56:44 -0700

    Add files via upload
    Debugged inventory2 file.

--- Main.java
+++ Main.java
@@ -1,39 +1,3 @@
-/*
-
-Main Class for Invertory Manager Program
-
-Author: Noah Pearson Kramer
-*/
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-
-
 public class Main{
 
-public static void main(String[] args) throws IOException{  
-System.out.println("\n Welcome to Inventory Manager V1.0 \n");
-System.out.println("Log in to get started: \n");
-        //Call Authentication class or method here
-UserAuthentication.main();
-int[] isAuthenticated = UserAuthentication.GetUserType();
-int authenticated = isAuthenticated[0]; 
-int userType = isAuthenticated[1];
-if (authenticated == 0){
-        System.out.println("\nAuthenticated\n");        
 }
-else{
-        System.out.println("Authentication failed, try again");
-}
-
-System.out.println("Enter local path of inventory file: ");
-BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in)); 
-String path = br1.readLine(); 
-InventoryManager.InventoryManager(path, authenticated, userType); 
-}
-}
-// call to inventory manager static method
-
-
-
